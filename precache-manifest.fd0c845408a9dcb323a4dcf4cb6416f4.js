c60f3216cda04362265e856fcdffebe3550e169d
CryNet <ucrynet@gmail.com>
2021-03-18 18:44:45 +0200

    feat: gp

---
+++ precache-manifest.fd0c845408a9dcb323a4dcf4cb6416f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2bc2998451b30a65216830c2fe6c1a",
+    "url": "/localhost/index.html"
+  },
+  {
+    "revision": "fd275823c18c4184af5c",
+    "url": "/localhost/static/css/main.6b410774.chunk.css"
+  },
+  {
+    "revision": "62c481f8e01937cf6e2f",
+    "url": "/localhost/static/js/2.94414032.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/localhost/static/js/2.94414032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd275823c18c4184af5c",
+    "url": "/localhost/static/js/main.8d12221e.chunk.js"
+  },
+  {
+    "revision": "f5d7e023bdb0499da9c2",
+    "url": "/localhost/static/js/runtime-main.7b88b5d6.js"
+  }
+]);
